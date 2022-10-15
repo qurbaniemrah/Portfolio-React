6f08d9185eff9056ba5760d853e73aba16213514
qurbaniemrah <qurbaniemrah@outlook.com>
2022-10-15 10:09:47 +0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Experience from './components/experience/Experience'
 import Services from './components/services/Services'
 import Portfolio from './components/portfolio/Portfolio'
 import Contact from './components/contact/Contact'
-import Footer from './components/footer/Footer'
+
 
 const App = () => {
   return (
@@ -18,7 +18,7 @@ const App = () => {
     <Services/>
     <Portfolio/>
     <Contact/>
-    <Footer/>
+
     </>
   )
 }
